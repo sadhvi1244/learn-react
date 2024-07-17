7254e470bfbbca4356a3c12a52e6a14fad8aa3ff
Sadhvi kesarwani <Sadhvikesarwani1@gmail.com>
2024-07-17 11:27:27 +0530

    basic react using vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+import Chai from "./chai";
+
+function App() {
+  return (
+    <>
+      <Chai />
+      <h1>I am a good person | i deserve everything</h1>
+      <p>
+        no matter what , one day i achieve everything , i deverse all the things
+        that i dream for.
+      </p>
+    </>
+  );
+}
+
+export default App;
