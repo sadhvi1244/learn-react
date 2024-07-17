9a9483904ac81b05080acac0f9d2245c16bf8b15
Sadhvi kesarwani <Sadhvikesarwani1@gmail.com>
2024-07-17 11:21:33 +0530

    Add 01basicreact folder

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Chai from "./Chai";
+
+function App() {
+  return (
+    <>
+      <Chai />
+      <h1>chai aur react | Sadhvi Kesarwani | I am leanrning react</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,5 @@
+function Chai() {
+  return <h1>Chai is not good for your health.</h1>;
+}
+
+export default Chai;
