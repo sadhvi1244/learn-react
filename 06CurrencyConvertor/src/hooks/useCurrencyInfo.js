85f93c950c0f6f2917584ecd8338db3d0ff64ce1
Sadhvi kesarwani <Sadhvikesarwani1@gmail.com>
2024-07-25 14:52:20 +0530

    Currency converter app

--- 06CurrencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06CurrencyConvertor/src/hooks/useCurrencyInfo.js
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency) {
-  const [data, setData] = useState({}); //empty object loop lgaige to crash nhi aaiga
+  const [data, setData] = useState({});
   useEffect(() => {
     fetch(
-      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/eur.json`
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
     )
-      .then((res) => res.json()) //json me convert krr rahe
+      .then((res) => res.json())
       .then((res) => setData(res[currency]));
     console.log(data);
   }, [currency]);
   console.log(data);
   return data;
 }
+
 export default useCurrencyInfo;
